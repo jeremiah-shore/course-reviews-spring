f916029d1d4ea5c306b35f06dd88768457ca89b5
Jeremiah Shore <shore.jeremiah@gmail.com>
2019-02-26 19:06:43 -0700

    added review entity

---
+++ src/main/java/net/jeremiahshore/review/Review.java
@@ -0,0 +1,36 @@
+package net.jeremiahshore.review;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private final Long id;
+    private int rating;
+    private String description;
+
+    //todo: reduce duplicated code for entities
+    public Review() {
+        id = null;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
