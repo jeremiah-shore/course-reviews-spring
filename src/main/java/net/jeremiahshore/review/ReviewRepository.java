861049458fc0909f747199fab17564069c890987
Jeremiah Shore <shore.jeremiah@gmail.com>
2019-02-27 00:00:17 -0700

    added authorization constraint so only the author of a review can delete it

--- src/main/java/net/jeremiahshore/review/ReviewRepository.java
+++ src/main/java/net/jeremiahshore/review/ReviewRepository.java
@@ -1,7 +1,16 @@
 package net.jeremiahshore.review;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 public interface ReviewRepository extends PagingAndSortingRepository<Review, Long> {
 
+    @Override
+    @PreAuthorize("@reviewRepository.findOne(#id)?.reviewer?.username == authentication.name")
+    void deleteById(@Param("id")Long id);
+
+    @Override
+    @PreAuthorize("#review.reviewer?.username == authentication.name")
+    void delete(@Param("review") Review entity);
 }
