5dbcad647ed115e68a4945169fa41f26a600150f
Jeremiah Shore <shore.jeremiah@gmail.com>
2019-02-26 23:26:53 -0700

    added user seed data

--- src/main/java/net/jeremiahshore/core/DatabaseLoader.java
+++ src/main/java/net/jeremiahshore/core/DatabaseLoader.java
@@ -3,31 +3,46 @@ package net.jeremiahshore.core;
 import net.jeremiahshore.course.Course;
 import net.jeremiahshore.course.CourseRepository;
 import net.jeremiahshore.review.Review;
+import net.jeremiahshore.user.User;
+import net.jeremiahshore.user.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.IntStream;
 
 @Component
 public class DatabaseLoader implements ApplicationRunner {
     private final CourseRepository courses;
+    private final UserRepository users;
+
+    private List<User> students;
 
     @Autowired
-    public DatabaseLoader(CourseRepository courses) {
+    public DatabaseLoader(CourseRepository courses, UserRepository users) {
         this.courses = courses;
+        this.users = users;
     }
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        seedSingleCourse();
-        seedBunchOfCourses();
+        seedSingleCourseData();
+        seedBulkCourseData();
+        seedSingleUserData();
+        seedBulkUserData();
     }
 
-    private void seedBunchOfCourses() {
+    private void seedSingleCourseData() {
+        Course course = new Course("Java Basics", "https://teamtreehouse.com/library/java-basics");
+        course.addReview(new Review(3, "It was decent."));
+        courses.save(course);
+    }
+
+    private void seedBulkCourseData() {
         String[] templates = {
                 "Up and Running with %s",
                 "%s Basics",
@@ -52,15 +67,34 @@ public class DatabaseLoader implements ApplicationRunner {
                     String title = String.format(template, buzzword);
 
                     Course c = new Course(title, "http://www.example.com");
-                    c.addReview(new Review(i % 5, String.format("Moar %s please!!", buzzword)));
+                    Review review = new Review((i % 5) + 1, String.format("Moar %s please!!", buzzword));
+                    review.setReviewer(students.get(i % students.size()));
+                    c.addReview(review);
                     bunchOfCourses.add(c);
                 });
         courses.saveAll(bunchOfCourses);
     }
 
-    private void seedSingleCourse() {
-        Course course = new Course("Java Basics", "https://teamtreehouse.com/library/java-basics");
-        course.addReview(new Review(3, "It was decent."));
-        courses.save(course);
+    private void seedBulkUserData() {
+        students = Arrays.asList(
+                new User("jacobproffer", "Jacob",  "Proffer", "password", new String[] {"ROLE_USER"}),
+                new User("mlnorman", "Mike",  "Norman", "password", new String[] {"ROLE_USER"}),
+                new User("k_freemansmith", "Karen",  "Freeman-Smith", "password", new String[] {"ROLE_USER"}),
+                new User("seth_lk", "Seth",  "Kroger", "password", new String[] {"ROLE_USER"}),
+                new User("mrstreetgrid", "Java",  "Vince", "password", new String[] {"ROLE_USER"}),
+                new User("anthonymikhail", "Tony",  "Mikhail", "password", new String[] {"ROLE_USER"}),
+                new User("boog690", "AJ",  "Teacher", "password", new String[] {"ROLE_USER"}),
+                new User("faelor", "Erik",  "Faelor Shafer", "password", new String[] {"ROLE_USER"}),
+                new User("christophernowack", "Christopher",  "Nowack", "password", new String[] {"ROLE_USER"}),
+                new User("calebkleveter", "Caleb",  "Kleveter", "password", new String[] {"ROLE_USER"}),
+                new User("richdonellan", "Rich",  "Donnellan", "password", new String[] {"ROLE_USER"}),
+                new User("albertqerimi", "Albert",  "Qerimi", "password", new String[] {"ROLE_USER"})
+        );
+        users.saveAll(students);
+    }
+
+    private void seedSingleUserData() {
+        User user = new User("jeremiahshore", "Jeremiah", "Shore", "password", new String[] {"ROLE_USER, ROLE_ADMIN"});
+        users.save(user);
     }
 }
