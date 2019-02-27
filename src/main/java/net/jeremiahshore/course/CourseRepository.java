2327517201ba172611d60ea475cc0f203e6378c0
Jeremiah Shore <shore.jeremiah@gmail.com>
2019-02-26 19:51:40 -0700

    converted model class repos from crud repo to page and sort repo

--- src/main/java/net/jeremiahshore/course/CourseRepository.java
+++ src/main/java/net/jeremiahshore/course/CourseRepository.java
@@ -1,7 +1,7 @@
 package net.jeremiahshore.course;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface CourseRepository extends CrudRepository<Course, Long> {
+public interface CourseRepository extends PagingAndSortingRepository<Course, Long> {
 
 }
