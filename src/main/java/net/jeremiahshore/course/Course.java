f63bfaf4d954a54cf848cb5067fc6352d6a8dcba
Jeremiah Shore <shore.jeremiah@gmail.com>
2019-02-26 22:55:24 -0700

    added RestConfig to enable validation and error messaging based on @NotNull annotation

--- src/main/java/net/jeremiahshore/course/Course.java
+++ src/main/java/net/jeremiahshore/course/Course.java
@@ -6,11 +6,13 @@ import net.jeremiahshore.review.Review;
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
+import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 public class Course extends BaseEntity {
+    @NotNull
     private String title;
     private String url;
     @OneToMany(mappedBy = "course", cascade = CascadeType.ALL)
