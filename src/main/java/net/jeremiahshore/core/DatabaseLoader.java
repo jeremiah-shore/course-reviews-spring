74187e9fb1cf03cae084d9b8d9b8a1d02212facb
Jeremiah Shore <shore.jeremiah@gmail.com>
2019-02-26 19:48:54 -0700

    added code to seed bulk amount of data at application load

--- src/main/java/net/jeremiahshore/core/DatabaseLoader.java
+++ src/main/java/net/jeremiahshore/core/DatabaseLoader.java
@@ -2,10 +2,15 @@ package net.jeremiahshore.core;
 
 import net.jeremiahshore.course.Course;
 import net.jeremiahshore.course.CourseRepository;
+import net.jeremiahshore.review.Review;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
 
 @Component
 public class DatabaseLoader implements ApplicationRunner {
@@ -18,7 +23,43 @@ public class DatabaseLoader implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
+        seedSingleCourse();
+        seedBunchOfCourses();
+    }
+
+    private void seedBunchOfCourses() {
+        String[] templates = {
+                "Up and Running with %s",
+                "%s Basics",
+                "%s for Beginners",
+                "%s for Neckbeards",
+                "Under the Hood: %s"
+        };
+        String[] buzzwords = {
+                "Spring REST",
+                "Java 9",
+                "Scala",
+                "Groovy",
+                "Hibernate"
+        };
+
+        List<Course> bunchOfCourses = new ArrayList<>();
+        IntStream.range(0, 100)
+                .forEach(i -> {
+                    String template = templates[i % templates.length];
+                    String buzzword = buzzwords[i % buzzwords.length];
+                    String title = String.format(template, buzzword);
+
+                    Course c = new Course(title, "http://www.example.com");
+                    c.addReview(new Review(i % 5, String.format("Moar %s please!!", buzzword)));
+                    bunchOfCourses.add(c);
+                });
+        courses.saveAll(bunchOfCourses);
+    }
+
+    private void seedSingleCourse() {
         Course course = new Course("Java Basics", "https://teamtreehouse.com/library/java-basics");
+        course.addReview(new Review(3, "It was decent."));
         courses.save(course);
     }
 }
